f8e9173201096a75bd768e5a86f84bb19657b760
wen <wisakura@outlook.com>
2022-05-07 15:49:54 +0800

    save

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,6 @@ const Document = () => {
 		<Html>
 			<Head>
 				<meta charSet='UTF-8' />
-				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link
 					rel='preconnect'
