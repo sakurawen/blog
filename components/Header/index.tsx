8253abaf3f3dcb7d2947b2afa05a2cbae3cea7ba
wen <wisakura@outlook.com>
2022-05-26 21:13:43 +0800

    更改首页样式

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,14 +3,24 @@ import Link from 'next/link';
 const Header = () => {
 	return (
 		<div>
-			<h1 className='text-5xl font-fusion mt-6 mb-2 font-extrabold leading-normal text-gray-800 '>
-				心酸小温的博客
-			</h1>
-			<p className='font-fusion mb-4 text-gray-600'>
-				日々私たちが过ごしている日常というのは
-				<br />
-				実は奇迹の连続なのかもしれん。
-			</p>
+			<div className='flex justify-between items-center'>
+				<div>
+					<h1 className='text-5xl font-fusion mt-6 mb-2 font-extrabold leading-normal text-gray-800 '>
+						心酸小温的博客
+					</h1>
+					<p className='font-fusion mb-4 text-gray-600'>
+						日々私たちが过ごしている日常というのは
+						<br />
+						実は奇迹の连続なのかもしれん。
+					</p>
+				</div>
+				<img
+					onDragStart={(e) => e.preventDefault()}
+					className='pointer-events-none mr-16 mt-4 select-none sm:block hidden rounded-full  w-32 h-32'
+					src='head.jpg'
+					alt=''
+				/>
+			</div>
 			<div className='flex font-fusion gap-4  text-lg text-gray-600'>
 				<Link passHref href={'/'}>
 					<a className='hover:text-black'>Posts</a>
