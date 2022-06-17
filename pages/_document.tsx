e12c2e0d6626215dc37879ef54c0366b6fc31258
wen <wisakura@outlook.com>
2022-06-17 19:32:50 +0800

    优化seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,10 +11,17 @@ const Document = () => {
 					href='https://fonts.gstatic.com'
 					crossOrigin='crossorigin'
 				/>
+				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
 				<link
 					href='https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;700&display=swap'
 					rel='stylesheet'
 				/>
+				<meta
+					name='keywords'
+					content='omazio,鼠人苦工的博客,blog,博客,鼠鼠,鼠人苦工,'
+				/>
+				<meta name='renderer' content='webkit' />
+				<meta name='description' content='鼠人苦工的博客' />
 			</Head>
 			<body>
 				<Main />
