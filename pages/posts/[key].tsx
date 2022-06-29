57a0d769f2ecd802cc76fc5dd2f334f82c04280e
wen <wisakura@outlook.com>
2022-06-29 19:11:34 +0800

    更新依赖

--- pages/posts/[key].tsx
+++ pages/posts/[key].tsx
@@ -26,7 +26,6 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 	const key = (ctx.params?.key as string) || '';
 	try {
 		const ret = await postsService.getDetail(key);
-
 		return {
 			props: {
 				data: ret.data,
@@ -54,62 +53,64 @@ const Posts: NextPage<{ data: Posts }> = ({ data }) => {
 		router.push('/');
 	};
 	return (
-		<Layout>
+		<>
 			<Head>
 				<title>{data.title} | 鼠人苦工的博客</title>
 			</Head>
-			<span
-				className='text-base font-fusion hover:px-2 group font-bold hover:bg-gray-100  text-black transition py-1  rounded mb-8 hidden md:inline-flex items-center cursor-pointer'
-				onClick={goBack}
-			>
-				<span className='mr-1 w-0 group-hover:w-5 opacity-0 group-hover:opacity-100 transition-all overflow-hidden'>
-					<svg
-						className='h-5 w-5 inline-block'
-						xmlns='http://www.w3.org/2000/svg'
-						aria-hidden='true'
-						role='img'
-						width='1em'
-						height='1em'
-						preserveAspectRatio='xMidYMid meet'
-						viewBox='0 0 24 24'
-					>
-						<path
-							fill='currentColor'
-							d='M20 11v2H8v2H6v-2H4v-2h2V9h2v2h12zM10 7H8v2h2V7zm0 0h2V5h-2v2zm0 10H8v-2h2v2zm0 0h2v2h-2v-2z'
-						/>
-					</svg>
+			<Layout>
+				<span
+					className='text-base font-fusion hover:px-2 group font-bold hover:bg-gray-100  text-black transition py-1  rounded mb-8 hidden md:inline-flex items-center cursor-pointer'
+					onClick={goBack}
+				>
+					<span className='mr-1 w-0 group-hover:w-5 opacity-0 group-hover:opacity-100 transition-all overflow-hidden'>
+						<svg
+							className='h-5 w-5 inline-block'
+							xmlns='http://www.w3.org/2000/svg'
+							aria-hidden='true'
+							role='img'
+							width='1em'
+							height='1em'
+							preserveAspectRatio='xMidYMid meet'
+							viewBox='0 0 24 24'
+						>
+							<path
+								fill='currentColor'
+								d='M20 11v2H8v2H6v-2H4v-2h2V9h2v2h12zM10 7H8v2h2V7zm0 0h2V5h-2v2zm0 10H8v-2h2v2zm0 0h2v2h-2v-2z'
+							/>
+						</svg>
+					</span>
+					<span>.. /</span>
 				</span>
-				<span>.. /</span>
-			</span>
-			<h1 className='text-4xl font-bold  font-fusion leading-sung'>
-				{data.title}
-			</h1>
-			<p className='mt-5 font-fusion mb-8 text-sm text-gray-500'>
-				{dayjs(data.create_time).format('YYYY / MM / DD')}
-			</p>
-			<div className='pb-12'>
-				{compiler(data.content, {
-					wrapper: 'article',
-					forceWrapper: true,
-					forceBlock: true,
-					overrides: mdxOverrides,
-				})}
-			</div>
-			<div className='mb-20'>
-				<Giscus
-					repo='sakurawen/blog-comments'
-					repoId='R_kgDOHhXNkw'
-					category='comments'
-					categoryId='DIC_kwDOHhXNk84CPvWs'
-					mapping='url'
-					reactionsEnabled='1'
-					emitMetadata='0'
-					inputPosition='bottom'
-					theme='light_tritanopia'
-					lang='zh-CN'
-				/>
-			</div>
-		</Layout>
+				<h1 className='text-4xl font-bold  font-fusion leading-sung'>
+					{data.title}
+				</h1>
+				<p className='mt-5 font-fusion mb-8 text-sm text-gray-500'>
+					{dayjs(data.create_time).format('YYYY / MM / DD')}
+				</p>
+				<div className='pb-12'>
+					{compiler(data.content, {
+						wrapper: 'article',
+						forceWrapper: true,
+						forceBlock: true,
+						overrides: mdxOverrides,
+					})}
+				</div>
+				<div className='mb-20'>
+					<Giscus
+						repo='sakurawen/blog-comments'
+						repoId='R_kgDOHhXNkw'
+						category='comments'
+						categoryId='DIC_kwDOHhXNk84CPvWs'
+						mapping='url'
+						reactionsEnabled='1'
+						emitMetadata='0'
+						inputPosition='bottom'
+						theme='light_tritanopia'
+						lang='zh-CN'
+					/>
+				</div>
+			</Layout>
+		</>
 	);
 };
 
