4b52047feaab32e7e2938acd7251af689c621c3e
wen <wen@wendeMacBook-Pro.local>
2024-11-23 19:28:12 +0800

    feat: update env keys

--- src/app/(app)/(home)/_components/hero.tsx
+++ src/app/(app)/(home)/_components/hero.tsx
@@ -6,14 +6,14 @@ export function Hero() {
     <div className='hero container min-h-screen mx-auto flex items-center justify-center'>
       <div className='flex flex-col w-full justify-center  items-center space-y-12'>
         <div>
-          <Image src='/image/avatar.jpg' width={320} height={320} className='rounded-full shadow-xl ring-1 ring-zinc-200' alt='avatar' />
+          <Image src='/image/avatar.jpg' width={220} height={220} className='rounded-full shadow-xl ring-1 ring-zinc-200' alt='avatar' />
         </div>
         <div className='text-center '>
-          <h1 className='text-4xl mb-4'>
+          <h1 className='text-3xl mb-4'>
             Hello ,I'm
             <span className='font-bold'> Wen</span>
           </h1>
-          <p className='text-4xl'>A NodeJS Full Stack Developer</p>
+          <p className='text-2xl'>A NodeJS Full Stack Developer</p>
         </div>
         <div className='flex justify-center'>
           <Social className='mt-4' />
--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,5 +1,5 @@
 import { NotionAPI } from 'notion-client';
 
 export const notion = new NotionAPI({
-  authToken: process.env.NEXT_PUBLIC_NOTION_KEY as string,
+  authToken: process.env.NOTION_KEY as string,
 });
