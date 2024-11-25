c2ca1321e6ffdf82a5cc37be9bff18bac26d95a5
wen <wisakura@outlook.com>
2024-11-25 22:42:43 +0800

    feat: remove animate

--- src/app/(app)/(home)/_components/avatar.tsx
+++ src/app/(app)/(home)/_components/avatar.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 export function Avatar() {
   return (
-    <m.div className='relative h-56 w-56' initial={{ opacity: 0, scale: 0, y: 100, x: -100 }} animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}>
+    <div className='relative h-56 w-56'>
       <Image src='/image/avatar.jpg' fill className='z-10 rounded-full shadow-xl pointer-events-none  ' alt='avatar' />
-      <Image src='/image/avatar.jpg' fill className='rounded-full opacity-60 blur-xl pointer-events-none' alt='avatar' />
-    </m.div>
+      <Image src='/image/avatar.jpg' fill className='rounded-full opacity-60 blur-xl pointer-events-none' alt='avatar shadow' aria-hidden />
+    </div>
   );
 }
--- src/app/(app)/blog/[id]/page.tsx
+++ src/app/(app)/blog/[id]/page.tsx
@@ -11,7 +11,7 @@ export default async function Post({ params }: { params: Promise<{ id: string }>
   const { id } = await params;
   const data = await getPost(id);
   return (
-    <div className='pt-24 max-w-2xl mx-auto'>
+    <div className='pt-24 max-w-2xl mx-auto sm:px-0 px-4'>
       <PostRenderer recordMap={data} fullPage disableHeader className='prose !max-w-full !px-0' />
     </div>
   );
