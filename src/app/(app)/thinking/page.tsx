470fcd0da1b3e029ae8dd17cb51963d5ec54baea
wen <wisakura@outlook.com>
2024-11-25 20:38:31 +0800

    chore:remove useless page

--- src/app/(app)/thinking/page.tsx
+++
@@ -1,5 +0,0 @@
-import { PageContainer } from '~/components/layout/page-container';
-
-export default function Thinking() {
-  return <PageContainer>thinking page</PageContainer>;
-}
--- src/app/(app)/timeline/page.tsx
+++
@@ -1,5 +0,0 @@
-import { PageContainer } from '~/components/layout/page-container';
-
-export default function Timeline() {
-  return <PageContainer>timeline page</PageContainer>;
-}
