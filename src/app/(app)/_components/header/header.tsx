c8783c465c132ba5dbf282f8e5dda412a5148db5
sakurawen <wisakura@outlook.com>
2025-01-23 23:38:48 +0800

    feat: update style

--- src/app/(app)/_components/header/header.tsx
+++ src/app/(app)/_components/header/header.tsx
@@ -1,17 +1,17 @@
-import { HeaderLink } from './header-link';
+import Link from 'next/link';
 import { HeaderMenu } from './header-menu';
 
 export function Header() {
   return (
-    <header className='header fixed top-0  w-full z-10 bg-white/80 backdrop-blur-lg'>
-      <div className='flex justify-between max-w-2xl mx-auto px-2 py-2.5'>
-        <div className='header-start text-sm space-x-2 flex items-center '>
-          <HeaderLink className='py-1 px-2.5 text-[15px] rounded-md overflow-hidden hover:bg-zinc-100' href='/'>
-            首页
-          </HeaderLink>
-          <HeaderLink className='py-1 px-2.5 text-[15px] rounded-md overflow-hidden hover:bg-zinc-100' href='/blog'>
-            博客
-          </HeaderLink>
+    <header className='header fixed top-0  w-full z-10 bg-white border-b'>
+      <div className='flex justify-between max-w-2xl mx-auto  py-4 px-4'>
+        <div className='header-start text-sm space-x-4 flex items-center '>
+          <Link className='text-sm rounded-md overflow-hidden text-gray-950' href='/'>
+            Home
+          </Link>
+          <Link className='text-sm rounded-md overflow-hidden text-gray-950' href='/blog'>
+            Blog
+          </Link>
         </div>
         <div className='header-end'>
           <HeaderMenu />
