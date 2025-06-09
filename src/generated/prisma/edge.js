5ba41592cc8ddc7f773c86ef95ea4b413e50ca6d
sakurawen <wisakura@outlook.com>
2025-06-09 16:52:50 +0800

    chore: remove postinstall flag from Prisma configuration

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -209,7 +209,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -210,7 +210,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
