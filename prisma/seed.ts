e2783e3c1adb771aace18ea3bde0467edd02be0a
sakurawen <wisakura@outlook.com>
2025-11-19 23:27:09 +0800

    feat: add Verification model with CRUD operations and aggregation capabilities

---
+++ prisma/seed.ts
@@ -0,0 +1,2 @@
+
+console.log("seed script running...");
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,16 @@
-import { PrismaClient } from '~/generated/prisma';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '~/generated/prisma/client';
+
 import 'server-only';
 
 let prismaClient: PrismaClient;
 
 export function prisma() {
   if (!prismaClient) {
-    prismaClient = new PrismaClient();
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    prismaClient = new PrismaClient({ adapter });
   }
   return prismaClient;
 }
